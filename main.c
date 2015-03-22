9b1944db4370016face67a0f75e4ca55ad24c138
YuriSS <yuri.s.dos.santos@gmail.com>
2015-03-21 22:11:40 -0300

    Adicionando funções para criar uma fila de árvores

--- main.c
+++ main.c
@@ -1,77 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "arvore.h"
-
-
-/* [INI] Fila de Arvores */
-	typedef struct Dado{
-		Arvore* tree;
-		struct Dado *prox, *ant;
-	}Dado;
-
-	typedef struct Fila{
-		Dado *inicio, *fim;
-	}Fila;
-
-	/* [INI] Inicializadores */
-		Dado* novoDado(Arvore* tree) {
-			Dado* novo = (Dado *)malloc(sizeof(Dado));
-			novo->tree = tree;
-			novo->prox = novo->ant = NULL;
-			return novo;
-		}
-
-		Fila* novaFila() {
-			Fila* nova = (Fila *)malloc(sizeof(Fila));
-			nova->inicio = nova->fim = NULL;
-			return nova;
-		}
-	/* [FIM] Inicializadores */
-
-
-	/* [INI] Insercao */
-		void pushFila(Fila* f, Arvore* tree) {
-			Dado* novo = novoDado(tree);
-			novo->ant = f->fim;
-			if(f->inicio == NULL)
-				f->inicio = novo;
-			else
-				f->fim->prox = novo;
-			f->fim = novo;
-		}
-	/* [FIM] Insercao */
-
-
-	/* [INI] Impressão */
-		void impressaoFila(Fila* f) {
-			Dado* aux = f->inicio;
-			int i = 1;
-			while(aux != NULL) {
-				printf("Case %d:\n", i);
-				impressoesArvore(aux->tree->root);
-				printf("\n");
-				aux = aux->prox;
-				i++;
-			}
-		}
-	/* [FIM] Impressão */
-/* [FIM] Fila de Arvores */
-
+#include "fila.h"
 
 int main() {
 
-	Arvore* tree = novaArvore(5);
-	pushTree(tree->root, 2);
-	pushTree(tree->root, 7);
-	Arvore* treeN = novaArvore(3);
-	pushTree(treeN->root, 19);
-	pushTree(treeN->root, 1);
-	pushTree(treeN->root, 25);
-	pushTree(treeN->root, 20);
 	Fila* fila = novaFila();
-	pushFila(fila, tree);
-	pushFila(fila, treeN);
-	printf("\n\n");
+	int testes, quant, num, i, j;
+	scanf("%d", &testes);
+
+	if(testes > 1000) return 0;
+	for(i=0; i<testes; i++) {
+		scanf("%d", &quant);
+		if(quant > 0 && quant < 500) {
+			scanf("%d", &num);
+			Arvore* tree = novaArvore(num);
+			for(j=0; j<quant - 1; j++) {
+				scanf("%d", &num);
+				pushTree(tree->root, num);
+			}
+			pushFila(fila, tree);
+		}
+	}
+
 	impressaoFila(fila);
 
 	return 0;

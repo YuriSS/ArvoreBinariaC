7c882d0c204b475dababc2eca62071ce0b88fe89
Yuri Santos <yuri.s.dos.santos@gmail.com>
2015-04-14 12:03:50 -0300

    Código compliado no URI

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "arvore.h"
-#include "fila.h"
 
 int main() {
-
-	Fila* fila = novaFila();
-	int testes, quant, num, i, j;
-	scanf("%d", &testes);
-
-	if(testes > 1000) return 0;
-	for(i=0; i<testes; i++) {
-		scanf("%d", &quant);
-		if(quant > 0 && quant < 500) {
-			scanf("%d", &num);
-			Arvore* tree = novaArvore(num);
-			for(j=0; j<quant - 1; j++) {
-				scanf("%d", &num);
-				pushTree(tree->root, num);
-			}
-			pushFila(fila, tree);
-		}
-	}
-
-	impressaoFila(fila);
-
-	return 0;
+    int numTestes, i;
+ 
+    scanf("%d", &numTestes);
+    if(numTestes > 1000)
+        return 0;
+ 
+    Tree* a[numTestes];
+    for( i=0; i<numTestes; i++ ) {
+        int n, j;
+        scanf("%d", &n);
+        if(n >= 1 && n <= 500) {
+            int valor;
+            for( j=0; j<n; j++ ) {
+                scanf("%d", &valor);
+                if(valor >= 0) {
+                    if(j == 0) {
+                        a[i] = novaTree(valor);
+                    } else {
+                        push(a[i]->root, valor);
+                    }
+                }
+            }
+        }
+    }
+ 
+    for( i=0; i<numTestes; i++ ) {
+        printf("Case %d:\n", i+1);
+        printf("Pre.:");
+        preorder(a[i]->root);
+        printf("\n");
+        printf("In..:");
+        inorder(a[i]->root);
+        printf("\n");
+        printf("Post:");
+        posorder(a[i]->root);
+        printf("\n\n");
+    }
+ 
+    return 0;
 }
